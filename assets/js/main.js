9312631e8c41ec4594000569159a50be1238077c
Yumna Ahmed <yuahmed1@davidson.edu>
2023-09-20 14:34:31 -0400

    halfway there

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,33 @@
 
 /* javascript */
+
+// 1 - This is just a test
+console.log("Hello from main.js");
+
+// 2 - Store a reference to the target element
+let subText = document.querySelector("h1");
+var swoosh = document.getElementById("swoosh");
+
+// 3 - Log the reference to confirm it works
+console.log(subText);
+
+// 4 - Add a mouseover event listener
+subText.addEventListener("mouseover", mouseoverHandler);
+
+// 5 - Create a handler function to run code when the event happens
+function mouseoverHandler() {
+    console.log(this.innerText);
+    //this.bswoosh.style.backgroundImage = "url(i.natgeofe.com/n/87d0e2f0-4583-4f6c-8a31-1766d41725a8/plastic-waste-single-use-worldwide-consumption-art-4a.jpg)";
+    swoosh.style.backgroundImage = "url(assets/img/pollbg.avif)";
+    swoosh.style.backgroundSize = "800px 800px"
+    this.innerText = "JUST POLLUTE IT.";
+    this.style = "cursor: wait"; // 7 - Update the cursor style
+}
+
+// 6 - An event listener and handler in the same statement
+subText.addEventListener("mouseout", function () {
+    this.innerText = "JUST DO IT.";
+    swoosh.style.backgroundImage = "url(assets/img/blackswooshx.png)";
+    swoosh.style.backgroundSize = "600px 600px"
+    this.style = "cursor: pointer";
+});
