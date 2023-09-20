f257ef52459154e22dec6e50a96c55153008b317
Yumna Ahmed <yuahmed1@davidson.edu>
2023-09-20 15:17:46 -0400

    latest, font color debugging

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ console.log("Hello from main.js");
 
 // 2 - Store a reference to the target element
 let subText = document.querySelector("h1");
-var swoosh = document.getElementById("swoosh");
+var swoosh = document.getElementById("bg");
 
 // 3 - Log the reference to confirm it works
 console.log(subText);
@@ -18,16 +18,20 @@ subText.addEventListener("mouseover", mouseoverHandler);
 function mouseoverHandler() {
     console.log(this.innerText);
     //this.bswoosh.style.backgroundImage = "url(i.natgeofe.com/n/87d0e2f0-4583-4f6c-8a31-1766d41725a8/plastic-waste-single-use-worldwide-consumption-art-4a.jpg)";
-    swoosh.style.backgroundImage = "url(assets/img/pollbg.avif)";
-    swoosh.style.backgroundSize = "800px 800px"
+
+    // swoosh.style.backgroundSize = "1500px 800px";
     this.innerText = "JUST POLLUTE IT.";
+    this.style.backgroundColor = "red";
+    swoosh.style.backgroundImage = "url(assets/img/pollbg.svg)";
+
     this.style = "cursor: wait"; // 7 - Update the cursor style
 }
 
 // 6 - An event listener and handler in the same statement
 subText.addEventListener("mouseout", function () {
     this.innerText = "JUST DO IT.";
-    swoosh.style.backgroundImage = "url(assets/img/blackswooshx.png)";
-    swoosh.style.backgroundSize = "600px 600px"
+    swoosh.style.backgroundColor = "white";
+    // swoosh.style.backgroundImage = "url(assets/img/blackswooshx.png)";
+    // swoosh.style.backgroundSize = "600px 600px";
     this.style = "cursor: pointer";
 });
