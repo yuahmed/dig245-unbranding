ac5b41fe824900c09012763b456a44367f0c0808
Yumna Ahmed <yuahmed1@davidson.edu>
2023-09-26 01:31:51 -0400

    completed changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,38 @@
 
 /* javascript */
 
-// 1 - This is just a test
-console.log("Hello from main.js");
-
-// 2 - Store a reference to the target element
 let subText = document.querySelector("h1");
-var swoosh = document.getElementById("bg");
+let bg = document.getElementById("bg");
+let swoosh = document.getElementById("swoosh");
+var bar = document.getElementById("bar");
 
-// 3 - Log the reference to confirm it works
-console.log(subText);
+//console.log(subText);
 
-// 4 - Add a mouseover event listener
-subText.addEventListener("mouseover", mouseoverHandler);
-
-// 5 - Create a handler function to run code when the event happens
-function mouseoverHandler() {
-    console.log(this.innerText);
-    //this.bswoosh.style.backgroundImage = "url(i.natgeofe.com/n/87d0e2f0-4583-4f6c-8a31-1766d41725a8/plastic-waste-single-use-worldwide-consumption-art-4a.jpg)";
-
-    // swoosh.style.backgroundSize = "1500px 800px";
-    this.innerText = "JUST POLLUTE IT.";
-    this.style.backgroundColor = "red";
-    swoosh.style.backgroundImage = "url(assets/img/pollbg.svg)";
-
-    this.style = "cursor: wait"; // 7 - Update the cursor style
+//message to tell user what to do to see transition
+function infoMessage(message){
+    alert(message);
 }
 
-// 6 - An event listener and handler in the same statement
-subText.addEventListener("mouseout", function () {
-    this.innerText = "JUST DO IT.";
-    swoosh.style.backgroundColor = "white";
-    // swoosh.style.backgroundImage = "url(assets/img/blackswooshx.png)";
-    // swoosh.style.backgroundSize = "600px 600px";
-    this.style = "cursor: pointer";
-});
+subText.addEventListener("click",onClickHandler);
+
+// handler function to transition upon click
+function onClickHandler() {
+    console.log(this.innerText);
+
+    // changing background image
+    //bg.style.backgroundSize = "1500px 800px";
+    bg.style.backgroundImage = "url(assets/img/pollbg.png)";
+
+    //changing tagline to subvert text
+    this.innerText = "JUST POLLUTE IT.";
+    this.style.color = "white";
+    this.style.fontSize = "130%";
+
+    //changing NIKE swoosh
+    swoosh.style.backgroundImage = "url(assets/img/whiteswooshx.png)";
+    swoosh.style.backgroundSize = "600px 500px";
+
+    //show bar with links
+    bar.style.display="block";
+    
+}
